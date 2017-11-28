45d2ec0b9e3323137eb9b254e348a457321aaf93
LysAMa <ladouceurlyse44@gmail.com>
2017-11-28 13:39:10 -0500

    menu update

--- Views/menu.php
+++ Views/menu.php
@@ -2,35 +2,14 @@
       <header id="header" class="hoc clear"> 
         <!-- ################################################################################################ -->
         <div id="logo" class="fl_left">
-          <h1><a href="index.php">Work With Data</a></h1>
+          <h1><a href="index.php">Data Share</a></h1>
         </div>
         <nav id="mainav" class="fl_right">
           <ul class="clear">
-            <li class="active"><a href="PHP/loginpage.php">Home</a></li>
-            <li><a class="drop" href="#">Pages</a>
-              <ul>
-                <li><a href="pages/gallery.html">Gallery</a></li>
-                <li><a href="pages/full-width.html">Full Width</a></li>
-                <li><a href="pages/sidebar-left.html">Sidebar Left</a></li>
-                <li><a href="pages/sidebar-right.html">Sidebar Right</a></li>
-                <li><a href="pages/basic-grid.html">Basic Grid</a></li>
-              </ul>
-            </li>
-            <li><a class="drop" href="#">Dropdown</a>
-              <ul>
-                <li><a href="#">Level 2</a></li>
-                <li><a class="drop" href="#">Level 2 + Drop</a>
-                  <ul>
-                    <li><a href="#">Level 3</a></li>
-                    <li><a href="#">Level 3</a></li>
-                    <li><a href="#">Level 3</a></li>
-                  </ul>
-                </li>
-                <li><a href="#">Level 2</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Apropos</a></li>
+            <li class="active"><a href="PHP/loginpage.php">Accueil</a></li>
+            <li><a href="#">A propos</a></li>
             <li><a href="#">Contact</a></li>
+            <li><a href="#">Supportez-nous!</a></li>
           </ul>
         </nav>
         <!-- ################################################################################################ -->
