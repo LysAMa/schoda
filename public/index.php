42accdf06b7be4c0bf7c610c333e783ca7450564
LysAMa <ladouceurlyse44@gmail.com>
2017-11-28 12:41:15 -0500

    suppression

--- public/index.php
+++ public/index.php
@@ -1,7 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 11/27/17
- * Time: 12:44 PM
- */
+//require'../pages/profil/header.html';
+require '../app/autoloader.php';
+//require '../app/app.php';
+
+App\autoloader::register();
+
+
+if (isset($_GET['p'])) {
+    $p=$_GET['p'];
+
+}
+
+else {
+
+    $p='home';
+}
+
+ob_start();
+//mb_internal_encoding('UTF-8');
+switch($p){
+    case 'home':
+        require '../pages/index.php';
+        break;
+    case 'about':
+        require '../pages/about.php';
+        break;
+    case 'support':
+        require '../pages/support.php';
+        break;
+    case 'contact':
+        require '../pages/contact.php';
+        break;
+    default :
+        require '../pages/index.php';
+
+}
+
+
+$content = ob_get_clean();
+require '../pages/template/default.php';
